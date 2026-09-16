bd051ca2dc226cc7f2448bf5551be3d62ba0a7e9
DYabsera <futurekingcyber@gmail.com>
2026-09-16 23:47:14 +0300

    Introduce Error Boundary

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,20 +3,26 @@ import { Header } from "./Header";
 import { Menu } from "./Menu";
 import { Footer } from "./Footer";
 import { Outlet, Link } from 'react-router-dom';
+import ErrorBoundary from "./ErrorBoundary";
+
 
 function Layout() {
   return (
+
     <div>
-        <div className="j">
+      <div className="j">
         <Header />
         <div className="Main">
           <div className="Main-overlay">
-            <Outlet/>
+            <ErrorBoundary>
+              <Outlet />
+            </ErrorBoundary>
           </div>
           <Footer />
         </div>
       </div>
     </div>
+
   )
 }
 
